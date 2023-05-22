e84e224356bed881c6328009667cdbd35ba1a03d
lisab <lisa.bergheaud@edu.ece.fr>
2023-05-22 19:43:46 +0200

    scolarite 2

--- Scolarite.php
+++ Scolarite.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html id="general">
 <head>
-	<link rel="stylesheet" type="text/css" href="Scolarite.css">
-	<link rel="stylesheet" type="text/css" href="footer.css">
-	<link rel="stylesheet" type="text/css" href="menu.css">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title> Scolarite </title>
+    <link rel="stylesheet" type="text/css" href="Scolarite.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    <script src="Scolarite.js"></script>
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Scolarite</title>
 </head>
 <body>
-	<?php include("menuadmin.php"); ?>
-    <div class ="tableau">
-
-    <?php
+    <?php include("menuadmin.php"); ?>
+    <div class="tableau">
+        <?php
         
         $servername = "localhost";
         $username = "root";
@@ -37,72 +38,98 @@
         $result_matieres = $conn->query($query_matieres);
 
         $conn->close();
-    ?>
-
-    <h2>Etudiants</h2>
-    <table>
-        <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-        </tr>
-        <?php
-        if ($result_etudiants->num_rows > 0) {
-            while ($row = $result_etudiants->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["Nom"] . "</td>";
-                echo "<td>" . $row["Prenom"] . "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='2'>Aucun étudiant trouvé</td></tr>";
-        }
         ?>
-    </table>
 
-    <h2>Professeurs</h2>
-    <table>
-        <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-        </tr>
-        <?php
-        if ($result_professeurs->num_rows > 0) {
-            while ($row = $result_professeurs->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["Nom"] . "</td>";
-                echo "<td>" . $row["Prenom"] . "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='2'>Aucun professeur trouvé</td></tr>";
-        }
-        ?>
-    </table>
+        <div>
+            <h2>Etudiants</h2>
+            <button onclick="addStudent()">
+                <img src="ajouter.png" alt="Ajouter">
+            </button>
+            <table>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                if ($result_etudiants->num_rows > 0) {
+                    while ($row = $result_etudiants->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["Nom"] . "</td>";
+                        echo "<td>" . $row["Prenom"] . "</td>";
+                        echo "<td>";
+                        echo "<img src='modifier.png' alt='Modifier' onclick='editStudent()'>";
+                        echo "<img src='supprimer.png' alt='Supprimer' onclick='deleteStudent()'>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>Aucun étudiant trouvé</td></tr>";
+                }
+                ?>
+            </table>
+        </div>
 
-    <h2>Matières</h2>
-    <table>
-        <tr>
-            <th>Nom</th>
-        </tr>
-        <?php
-        if ($result_matieres->num_rows > 0) {
-            while ($row = $result_matieres->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["Nom"] . "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td>Aucune matière trouvée</td></tr>";
-        }
-        ?>
+        <div>
+            <h2>Professeurs</h2>
+            <button onclick="addProfessor()">
+                <img src="ajouter.png" alt="Ajouter">
+            </button>
+            <table>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                if ($result_professeurs->num_rows > 0) {
+                    while ($row = $result_professeurs->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["Nom"] . "</td>";
+                        echo "<td>" . $row["Prenom"] . "</td>";
+                        echo "<td>";
+                        echo "<img src='modifier.png' alt='Modifier' onclick='editProfessor()'>";
+                        echo "<img src='supprimer.png' alt='Supprimer' onclick='deleteProfessor()'>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>Aucun professeur trouvé</td></tr>";
+                }
+                ?>
+            </table>
+        </div>
+
+        <div>
+            <h2>Matières</h2>
+            <button onclick="addSubject()">
+                <img src="ajouter.png" alt="Ajouter">
+            </button>
+            <table>
+                <tr>
+                    <th>Nom</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                if ($result_matieres->num_rows > 0) {
+                    while ($row = $result_matieres->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["Nom"] . "</td>";
+                        echo "<td>";
+                        echo "<img src='modifier.png' alt='Modifier' onclick='editSubject()'>";
+                        echo "<img src='supprimer.png' alt='Supprimer' onclick='deleteSubject()'>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='2'>Aucune matière trouvée</td></tr>";
+                }
+                ?>
+            </table>
+        </div>
+        
     </div>
 
     <?php include("footer.php"); ?>
 </body>
 </html>
-
-
-	
-	
-
-
