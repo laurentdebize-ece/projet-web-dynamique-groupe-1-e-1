1b3ef30a67f439ba2b3b3a354029ce5fe86ff6dd
lisab <lisa.bergheaud@edu.ece.fr>
2023-05-23 13:22:34 +0200

    scolarite java

--- Scolarite.js
+++ Scolarite.js
@@ -1,9 +1,7 @@
 function addStudent() {
-        // Récupérer les données du nouvel étudiant depuis un formulaire
         var nom = "Nouveau Nom de l'étudiant";
         var prenom = "Nouveau Prénom de l'étudiant";
 
-        // Envoyer les données à la base de données et actualiser le tableau
         // Exemple de requête AJAX avec jQuery :
         $.post("Scolarite.php", { action: "addStudent", nom: nom, prenom: prenom }, function(data) {
             // Actualiser le tableau des étudiants
@@ -12,14 +10,10 @@ function addStudent() {
     }
 
     function editStudent() {
-        // Récupérer les données de l'étudiant à modifier depuis le tableau
         var nom = "Nom de l'étudiant à modifier";
         var prenom = "Prénom de l'étudiant à modifier";
 
-        // Afficher un formulaire de modification avec les données existantes
-        // et permettre à l'utilisateur de saisir les nouvelles valeurs
 
-        // Envoyer les données modifiées à la base de données et actualiser le tableau
         // Exemple de requête AJAX avec jQuery :
         $.post("Scolarite.php", { action: "editStudent", nom: nom, prenom: prenom }, function(data) {
             // Actualiser le tableau des étudiants
@@ -28,18 +22,45 @@ function addStudent() {
     }
 
     function deleteStudent() {
-        // Récupérer les données de l'étudiant à supprimer depuis le tableau
         var nom = "Nom de l'étudiant à supprimer";
         var prenom = "Prénom de l'étudiant à supprimer";
 
-        // Afficher une confirmation de suppression à l'utilisateur
+       
 
-        // Supprimer l'étudiant de la base de données et actualiser le tableau
-        // Exemple de requête AJAX avec jQuery :
+     
         $.post("Scolarite.php", { action: "deleteStudent", nom: nom, prenom: prenom }, function(data) {
             // Actualiser le tableau des étudiants
             console.log("Étudiant supprimé !");
         });
     }
 
-    // Les fonctions pour les professeurs et les matières peuvent être définies de manière similaire
+
+
+    function showForm(formId) {
+        // Masquer formulaires
+        var forms = document.getElementsByClassName("add-form");
+        for (var i = 0; i < forms.length; i++) {
+            forms[i].style.display = "none";
+        }
+    
+    
+        var form = document.getElementById(formId);
+        form.style.display = "block";
+    }
+    
+
+
+const addStudentForm = document.getElementById('addStudentForm');
+
+
+
+addStudentForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+
+    const studentName = document.getElementById('studentName').value;
+    const studentFirstName = document.getElementById('studentFirstName').value;
+
+ 
+    addStudentForm.reset();
+});
