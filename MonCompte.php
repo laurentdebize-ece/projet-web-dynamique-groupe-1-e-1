8423ada9178a0c0ed2f54ba611866b11dea5fbbd
romaindiette <romain.diette@edu.ece.fr>
2023-05-23 20:52:52 +0200

    bavv

--- MonCompte.php
+++ MonCompte.php
@@ -1,82 +1,88 @@
 <!DOCTYPE html>
 <html id="general">
 
-
 <head>
-    <link rel="stylesheet" type="text/css" href="MonCompte.css">
-    <link rel="stylesheet" type="text/css" href="footer.css">
-    <link rel="stylesheet" type="text/css" href="menu.css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mon Compte</title>
+  <link rel="stylesheet" type="text/css" href="MonCompte.css">
+  <link rel="stylesheet" type="text/css" href="footer.css">
+  <link rel="stylesheet" type="text/css" href="menu.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <title>Mon Compte</title>
 </head>
 
-
 <body>
-    <?php
-    session_start();
-    $typeUtilisateur = $_SESSION["typeUtilisateur"];
-    if ($typeUtilisateur == "admin") {
-        include("menuadmin.php");
+  <?php
+  session_start();
+  $typeUtilisateur = $_SESSION["typeUtilisateur"];
+  if ($typeUtilisateur == "admin") {
+    include("menuadmin.php");
+  } else {
+    include("menu.php");
+  }
+  ?>
+  <div class="info">
+    <p> Informations du compte : </p> <br>
+    <ul>
+      <li> Type d'utilisateur :
+        <?php echo "$typeUtilisateur"; ?>
+      </li>
+      <li> Nom :
+        <?php echo $_SESSION["nom"]; ?>
+      </li>
+      <li>Prénom :
+        <?php echo $_SESSION["prenom"]; ?>
+      </li>
+      <li>Mail :
+        <?php echo $_SESSION["mail"]; ?>
+      </li>
+    </ul>
+    <img src="parametre.png" alt="Image parametre" class="small-image">
+  </div>
+  <?php include("footer.php"); ?>
+  <div class="rectangle-container">
+    <div class="rectangle">
+      <p>Changer le mot de passe </p>
+      <div class="image-carree"></div>
+    </div>
+    <div class="rectangle 2">
+      <p>Changer l'adresse mail</p>
+      <div class="image-carree"></div>
+    </div>
+  </div>
+
+  <?php
+  // Connexion à la base de données
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "Omnes MySkills";
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $newPassword = $_POST["password"];
+    $confirmedPassword = $_POST["confirm-password"];
+
+ 
+    if ($newPassword == $confirmedPassword) {
+      
+      $sql = "UPDATE nom_table SET MotDePasse = '$newPassword' WHERE IdEtudiant =  '$_SESSION[id_etudiant]'";
+      if ($conn->query($sql) === TRUE) {
+        echo "Mot de passe mis à jour avec succès !";
+      } else {
+        echo "Erreur lors de la mise à jour du mot de passe : " . $conn->error;
+      }
     } else {
-        include("menu.php");
+      echo "Les mots de passe ne correspondent pas !";
     }
-    ?>
-    <div class="info">
-        <p> Informations du compte : </p> <br>
-        <ul>
-            <li> Type d'utilisateur :
-                <?php echo "$typeUtilisateur"; ?>
-            </li>
-            <li> Nom :
-                <?php echo $_SESSION["nom"]; ?>
-            </li>
-            <li>Prénom :
-                <?php echo $_SESSION["prenom"]; ?>
-            </li>
-            <li>Mail :
-                <?php echo $_SESSION["mail"]; ?>
-            </li>
-        </ul>
-        <img src="parametre.png" alt="Image parametre" class="small-image">
-    </div>
-    <?php include("footer.php"); ?>
+  }
 
-
-
-
-    <div class="rectangle-container">
-        <div class="rectangle">
-            <p>Changer le mot de passe </p>
-            <div class="image-carree"></div>
-        </div>
-        <div class="rectangle">
-            <p>Changer l'adresse mail</p>
-            <div class="image-carree"></div>
-        </div>
-    </div>
-
-
- <?php
- if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Inclusion des fichiers
-    include 'fichier1.php';
-    include 'fichier2.php';
-   
-    // Contenu de fichier1.php
-    // ...
-   
-    // Contenu de fichier2.php
-    // ...
-   
-    // Fermeture des fichiers ouverts
-    fclose($fichier1);
-    fclose($fichier2);
-}
-?>
-
-
+  $conn->close();
+  ?>
 </body>
-
 
 </html>
