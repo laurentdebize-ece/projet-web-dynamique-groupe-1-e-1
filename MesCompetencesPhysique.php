655204476da1a9d71ea3bca767b961a6ae92a513
NinoRivoiron <92568944+NinoRivoiron@users.noreply.github.com>
2023-05-23 23:36:40 +0200

    Update MesCompetencesPhysique.php

--- MesCompetencesPhysique.php
+++ MesCompetencesPhysique.php
@@ -24,35 +24,40 @@
 			</thead>
 			<tbody>
 				<?php
-				// Connexion à la base de données
+
 				$servername = "localhost";
 				$username = "root";
 				$password = "root";
 				$dbname = "Omnes MySkills";
-
+				
 				$conn = new mysqli($servername, $username, $password, $dbname);
 
-				// Vérifier la connexion
+				session_start();
+
 				if ($conn->connect_error) {
 					die("Échec de la connexion : " . $conn->connect_error);
 				}
 
-				// Requête SQL pour récupérer les compétences triées par matière
-				$sql = "SELECT Competences.Nom, Matieres.Nom AS MatiereNom
-                    FROM Competences
-                    INNER JOIN Matieres ON Competences.IdMatiere = Matieres.IdMatieres
-                    WHERE Matieres.Nom = 'Physique'
-                    ORDER BY Matieres.Nom ASC";
+				$sql = "SELECT Competences.Nom, Matieres.Nom AS MatiereNom, Niveau.Etat AS Eval, Evaluations.IdEtudiant AS Id
+                    			FROM Competences
+                    			INNER JOIN Matieres ON Competences.IdMatiere = Matieres.IdMatieres
+					INNER JOIN Evaluations on Evaluations.IdCompetences = Competences.IdCompetences
+					INNER JOIN Niveau ON Niveau.IdNiveau = Evaluations.IdNiveau
+					INNER JOIN Etudiants on Etudiants.IdEtudiant = Evaluations.IdEtudiant
+					WHERE Matieres.Nom = 'Physique'
+                    			ORDER BY Matieres.Nom ASC";
 
 				$result = $conn->query($sql);
 
 				if ($result->num_rows > 0) {
 					while ($row = $result->fetch_assoc()) {
-						echo "<tr>";
-						echo "<td>" . $row["Nom"] . "</td>";
-						echo "<td>" . $row["MatiereNom"] . "</td>";
-						echo "<td>","Non évalué","</td>";
-						echo "</tr>";
+						if($_SESSION["id_etudiant"]==$row["Id"]){
+							echo "<tr>";
+							echo "<td>" . $row["Nom"] . "</td>";
+							echo "<td>" . $row["MatiereNom"] . "</td>";
+							echo "<td>" . $row["Eval"] . "</td>";
+							echo "</tr>";
+						}
 					}
 				} else {
 					echo "<tr><td colspan='2'>Aucune compétence trouvée</td></tr>";
